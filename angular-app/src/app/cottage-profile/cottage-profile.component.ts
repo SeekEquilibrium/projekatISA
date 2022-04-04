59be7e8490916d348f3b19e63442d8c0f798116d
SrdjanBeric <srdjan.beric123@gmail.com>
2022-04-04 17:08:22 +0200

    redirect if cottage in url doesn't exist

--- angular-app/src/app/cottage-profile/cottage-profile.component.ts
+++ angular-app/src/app/cottage-profile/cottage-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ConfigService } from '../service';
 import { CottageService } from '../service/cottage.service';
 import { CottageProfile } from '../shared/cottageProfile';
@@ -14,20 +14,27 @@ export class CottageProfileComponent implements OnInit {
   cottageProfile: CottageProfile;
   cottageOwner: any;
   imagePath: string;
+  returnUrl: string;
 
   constructor(
     private route:ActivatedRoute,
     private cottageService: CottageService,
-    private configService: ConfigService
+    private configService: ConfigService,
+    private router: Router,
+
     ) { }
 
   ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     this.imagePath = this.configService.image_path()
     this.cottageService.getCottageProfile(this.route.snapshot.paramMap.get('name')).subscribe(
       response => {
         this.cottageProfile = response;
         this.cottageOwner = this.cottageProfile.cottageOwner;
         console.log(this.cottageProfile)
+      },
+      error => {
+        this.router.navigate([this.returnUrl]);
       }
     )
   }
